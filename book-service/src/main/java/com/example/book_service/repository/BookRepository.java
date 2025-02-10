1c36fa88dcaee07f60d68880c3785ef4083a754d
vigneshwaripalani <59059278+vigneshwaripalani@users.noreply.github.com>
2025-02-10 18:07:04 +0530

    Delete book-service directory

--- book-service/src/main/java/com/example/book_service/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.book_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.book_service.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, String>{
-
-}
