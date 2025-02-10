a2c3060d07de57db6bc444c7ce3a47bfbc13ba1e
vigneshwaripalani <59059278+vigneshwaripalani@users.noreply.github.com>
2025-02-10 18:11:07 +0530

    Delete subscription-service directory

--- subscription-service/src/main/java/com/example/subscription_service/exception/BookNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.example.subscription_service.exception;
-
-public class BookNotAvailableException extends RuntimeException {
-    public BookNotAvailableException(String message) {
-        super(message);
-    }
-}
